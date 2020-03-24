313d5d1536465664dc5abb1f5fc90efcc7712ceb
Nicola Sartorato <97nico.s@gmail.com>
2020-03-24 18:32:03 +0100

    Uso nuovi casi (differenza tra dati adiacenti totale_casi) anziche' casi_attualmente_positivi

--- plot_maker.py
+++ plot_maker.py
@@ -33,8 +33,11 @@ if plot_type == 0:
     # ottengo gioni
     giorni = list(map(parse_data, regione_csv["data"].values))
     
-    # nuovi attualmente positivi all'interno della regione
-    nuovi_positivi = regione_csv["nuovi_attualmente_positivi"].values
+    # nuovi casi all'interno della regione
+    totale_casi = regione_csv["totale_casi"].values
+    nuovi_casi_positivi = [totale_casi[0]]
+    for g in range(len(totale_casi) - 1):
+        nuovi_casi_positivi += [totale_casi[g+1] - totale_casi[g]]
     
     # ottengo il numero dei nuovi tamponi giornalieri
     numero_tamponi = regione_csv["tamponi"].values
@@ -44,7 +47,7 @@ if plot_type == 0:
     
     # percentuale nuovi_positivi/nuovi_tamponi
     div = lambda x: x[0]/x[1] if 0<(x[0]/x[1])<1 else None
-    perc_nuovi_pos_nuovi_tamp = list(map(div, list(zip(nuovi_positivi, delta_tamponi))))
+    ratio_nuovi_pos_nuovi_tamp = list(map(div, list(zip(nuovi_casi_positivi, delta_tamponi))))
     
     # --------------- END OBTAIN DATA ---------------------------
     w = 0.3
@@ -54,24 +57,24 @@ if plot_type == 0:
     
     ax1.set_xlabel("Giorni")
     ax1.set_ylabel("Unità")
-    bar_pos = ax1.bar(ind+w, nuovi_positivi, color="tab:red", width=w)
+    bar_pos = ax1.bar(ind+w, nuovi_casi_positivi, color="tab:red", width=w)
     bar_tamp = ax1.bar(ind, delta_tamponi, color="tab:grey", width=w)
     ax1.tick_params(axis='y')
     
     ax1.set_xticks(ind+(w/2))
     ax1.set_xticklabels(giorni)
-    ax1.legend( (bar_pos, bar_tamp), ('Nuovi positivi', 'Nuovi tamponi') )
+    ax1.legend( (bar_pos, bar_tamp), ('Nuovi casi positivi', 'Nuovi tamponi') )
     ax1.set_xticks([])
 
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
     
     color = 'tab:blue'
-    ax2.set_ylabel('Nuovi positivi / nuovi tamponi', color=color)  # we already handled the x-label with ax1
-    line_perc = ax2.plot(giorni, perc_nuovi_pos_nuovi_tamp, color=color)
+    ax2.set_ylabel('Nuovi casi positivi / nuovi tamponi (al giorno)', color=color)  # we already handled the x-label with ax1
+    line_perc = ax2.plot(giorni, ratio_nuovi_pos_nuovi_tamp, color=color)
     ax2.tick_params(axis='y', labelcolor=color)
     
     fig.tight_layout()  # otherwise the right y-label is slightly clipped
     fig.set_size_inches(20, 5)
-    plt.title(interested_region + ": confronto giornaliero tra nuovi positivi e nuovi tamponi")
+    plt.title(interested_region + ": confronto giornaliero tra nuovi casi positivi e nuovi tamponi")
     plt.show()
         
